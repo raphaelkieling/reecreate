3deeb6e530706332a43faa29222b05eeb9a6f91c
Raphael Kieling <rapha_kieling@hotmail.com>
2021-03-21 22:19:21 -0300

    :sparkles: Createa the deepClone and some tests

--- src/core.test.ts
+++ src/core.test.ts
@@ -1,8 +1,90 @@
 import Recreate from "./index";
 
 describe("Core", () => {
-  test("Create a function with the same interface that a reducer", () => {
-    const result = Recreate.reducer(() => {});
-    expect(result).toBeTruthy();
+  describe("reducer", () => {
+    test("create a function with the same interface that a reducer", () => {
+      const result = Recreate.reducer(() => {});
+      expect(result).toBeTruthy();
+      expect(typeof result).toBe("function");
+    });
+
+    test("should return with a new object when send a action using the reducer", () => {
+      const reducerForRedudex = Recreate.reducer((draft, action) => {
+        if (action.type === "add_job") {
+          draft.jobs.push(action.payload);
+          return draft;
+        }
+      });
+
+      const initialState = { name: "Test", jobs: [] };
+      const action = {
+        type: "add_job",
+        payload: {
+          name: "TEST_JOB",
+        },
+      };
+
+      const result = reducerForRedudex(initialState, action);
+
+      expect(result).toBeTruthy();
+      expect(result).not.toMatchObject(initialState);
+      expect(result.jobs).not.toBe(initialState.jobs);
+    });
+  });
+
+  describe("deepCloneValue", () => {
+    test("Clone a simple object", () => {
+      const mainObject = { name: "" };
+      const result = Recreate.deepCloneValue(mainObject);
+      expect(mainObject).not.toBe(result);
+    });
+
+    test("should return the value if not is a object value", () => {
+      const resultNull = Recreate.deepCloneValue(null);
+      expect(resultNull).toBe(null);
+
+      const resultUndefined = Recreate.deepCloneValue(undefined);
+      expect(resultUndefined).toBe(undefined);
+
+      const resultEmptyObject = Recreate.deepCloneValue({});
+      expect(resultEmptyObject).toBe(resultEmptyObject);
+
+      const resultPrimitiveValue = Recreate.deepCloneValue(1);
+      expect(resultPrimitiveValue).toBe(1);
+    });
+
+    test("Clone a object deep object, depth = 1", () => {
+      const deepObject = { deep: true };
+      const mainObject = { name: {}, deepObject };
+      const result = Recreate.deepCloneValue(mainObject);
+
+      expect(mainObject).not.toBe(result);
+      expect(mainObject.deepObject).not.toBe(result.deepObject);
+      expect(result).toMatchObject(mainObject);
+    });
+
+    test("Clone a object deep object, depth > 1", () => {
+      const mainObject = {
+        name: {},
+        deep1: {
+          deep2: {
+            deep3: {
+              simpleArray: [1, 2, 3],
+              complexArray: [
+                {
+                  name: "advanced",
+                },
+              ],
+              active: true,
+            },
+          },
+        },
+      };
+      const result = Recreate.deepCloneValue(mainObject);
+
+      expect(mainObject).not.toBe(result);
+      expect(mainObject.deep1).not.toBe(result.deep1);
+      expect(result).toMatchObject(mainObject);
+    });
   });
 });
--- src/core.ts
+++ src/core.ts
@@ -20,26 +20,51 @@
  *
  * Output: (initial, action)=>{}
  */
-type ReducerClone = (state: { [key: string]: any }, action: any) => any;
-type ReducerType = (draft: { [key: string]: any }, action: any) => void;
+type SimpleObject = { [key: string]: any };
+type ReducerClone = (state: SimpleObject, action: any) => any;
+type ReducerType = (draft: SimpleObject, action: any) => void;
 
+type DeepCloneReturn = any;
+type DeepCloneParams = any;
+// Clone the object to avoid references
+function deepCloneValue(valueObject: DeepCloneParams): DeepCloneReturn {
+  if (valueObject === null) return null;
+  if (valueObject === undefined) return undefined;
+  if (typeof valueObject !== "object") return valueObject;
+
+  const finalObjectKeys = Object.keys(valueObject);
+
+  // Recursive way to create a clone of objects
+  const finalObject = finalObjectKeys.reduce(
+    (acc: SimpleObject, currentKey: string): SimpleObject => {
+      const value = valueObject[currentKey];
+      const typeOfValue = typeof value;
+
+      if (Array.isArray(value)) {
+        acc[currentKey] = value.map(deepCloneValue);
+      } else if (typeOfValue === "object") {
+        acc[currentKey] = deepCloneValue(value);
+      } else {
+        acc[currentKey] = value;
+      }
+
+      return acc;
+    },
+    {}
+  );
+
+  return finalObject;
+}
+
+// Create a reducer function
 function reducer(callback: ReducerType): ReducerClone {
   return (state, action) => {
-    callback(state, action);
+    // make the deep clone
+    return callback(deepCloneValue(state), action);
   };
 }
 
 export default {
   reducer,
+  deepCloneValue,
 };
-
-// export default function appReducer(state = initialState, action) {
-//     // The reducer normally looks at the action type field to decide what happens
-//     switch (action.type) {
-//       // Do something here based on the different types of actions
-//       default:
-//         // If this reducer doesn't recognize the action type, or doesn't
-//         // care about this specific action, return the existing state unchanged
-//         return state
-//     }
-//   }
